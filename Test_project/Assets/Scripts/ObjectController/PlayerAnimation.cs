92f7e110743a8ed392b2807e328b4d3604f637ea
donghwa722 <donghwa722@snu.ac.kr>
2025-06-06 18:16:46 +0900

    fix #69, #70 - refix

--- Test_project/Assets/Scripts/ObjectController/PlayerAnimation.cs
+++ Test_project/Assets/Scripts/ObjectController/PlayerAnimation.cs
@@ -19,7 +19,7 @@ public class PlayerAnimation : MonoBehaviour
 
     void Update()
     {
-        float speed = Input.GetAxis("Vertical");
+        float speed = Mathf.Abs(Input.GetAxis("Vertical"));
 
         animator.SetBool("Static_b", true);
         animator.SetFloat("Speed_f", speed);
